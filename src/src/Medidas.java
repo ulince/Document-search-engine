77cc1581baff49bc313901c95d280c6f3f71ae84
Ulysses <lincerom.ulys@uwlax.edu>
2013-10-09 13:09:35 -0500

    Commit de prueba

--- src/src/Medidas.java
+++ src/src/Medidas.java
@@ -1,5 +1,5 @@
 /*
- * Esta clase contiene 
+ * Esta clase contiene las operaciones de las medidas de similtud y disimilitud entre matrices.
  */
 package src;
 
@@ -9,4 +9,34 @@ package src;
  */
 public class Medidas {
     
+    public Medidas(){
+        
+    }
+    
+    //Calcula el producto interno entre 2 documentos
+    //La matriz que se pasa es dT, resultado de la aplicación de la técnica LSI a
+    //La matriz tiene dimension 10 x k, donde k es...
+    public float ProdInterno2Doc(float[][] dT, int docA, int docB){
+        float result = 0;
+        
+        for(int i = 0; i < 10; i++){
+            result = result + dT[i][docA]*dT[i][docB];
+        }
+        return result;
+    }
+    
+     /*public float Coseno2Doc(float[][] dT, int docA, int docB){
+        float numerador = 0;
+        float temp1 = 0;
+        float temp2 = 0;
+        
+        for(int i = 0; i < 10; i++){
+            numerador = numerador + dT[i][docA]*dT[i][docB];
+        }
+        
+        for(int i = 0; i < 10; i++){
+            temp1 = temp1 + 
+        }      
+    }*/
+     
 }
